3f1f587a55ad3128ba02b3ef6214ebf63065da74
jaswanth200 <55279054+jaswanth200@users.noreply.github.com>
2024-04-01 19:27:30 +0530

    Update script.sh

--- method1/script.sh
+++ method1/script.sh
@@ -10,19 +10,19 @@ git clone https://github.com/TekspotEdu/microserviceapp.git
 #       if [[ $2 -eq "demo-frontend" && "demo-backend1" && "demo-backend2" ]]; then
 case $2 in
 
-        "demo-frontend")
+        "react")
                 cd /home/ubuntu/microserviceapp/demo-frontend
                 sudo docker build -t docker_frontendimg . -f Dockerfile
                 sudo docker run -d docker_frontendimg
         ;;
 
-        "demo-backend1")
+        "java")
                 cd /home/ubuntu/microserviceapp/demo-backend1
                 sudo docker build -t docker_backend1img . -f Dockerfile
                 sudo docker run docker_backend1img
         ;;
 
-        "demo-backend2")
+        "python")
                 cd /home/ubuntu/microserviceapp/demo-backend2
                 sudo docker build -t docker_backend2img . -f Dockerfile
                 sudo docker run docker_backend2img
@@ -36,7 +36,7 @@ else
         #if [[ $2 -eq "demo=frontend" && "demo-backend1" && "demo-backend2" ]]; then
 case $2 in
 
-        "demo-frontend")
+        "react")
         #   ssh ubuntu@$3 "sudo apt-get update && apt-get install -y npm"
                 ssh ubuntu@$3 "sudo git clone "https://github.com/TekspotEdu/microserviceapp.git" "
                 ssh ubuntu@$3 "sudo apt-get update && sudo apt-get install -y nginx"
@@ -47,14 +47,14 @@ case $2 in
 
         ;;
 
-        "demo-backend1")
+        "java")
                 ssh ubuntu@$3 "sudo apt-get update && sudo apt-get install -y openjdk-11-jdk"
                 ssh ubuntu@$3 "sudo git clone "https://github.com/TekspotEdu/microserviceapp.git" "
                 ssh ubuntu@$3 "cd /home/ubuntu/microserviceapp/demo-backend1/target/ && sudo java -jar sentiment-analysis-web-0.0.2-SNAPSHOT.jar --sa.logic.api.url http://$3:5000"
 
          ;;
-
-        "demo-backend2")
+         
+         "python")
                 ssh ubuntu@$3 "sudo apt-get update && sudo apt-get install -y python3"
                 ssh ubuntu@$3 "sudo apt-get update && sudo apt-get install -y python3-pip"
                 ssh ubuntu@$3 "sudo git clone "https://github.com/TekspotEdu/microserviceapp.git" "
