6b7c5f6acd3370d13b5891bd06d689810d08ce7f
jaswanth200 <55279054+jaswanth200@users.noreply.github.com>
2024-04-01 22:49:53 +0530

    Update script1.sh

--- method2/script1.sh
+++ method2/script1.sh
@@ -38,19 +38,19 @@ bash  docker_package
 case $service in
 
 "react")
-        cd /home/ubuntu/microserviceapp/demo-frontend
+        cd microserviceapp/demo-frontend
         sudo docker build -t docker_frontendimg . -f Dockerfile
         sudo docker run docker_frontendimg
 ;;
 
 "java")
-        cd /home/ubuntu/microserviceapp/demo-backend1
+        cd microserviceapp/demo-backend1
         sudo docker build -t docker_backend1img . -f Dockerfile
         sudo docker run docker_backend1img
 ;;
 
 "python")
-        cd /home/ubuntu/microserviceapp/demo-backend2
+        cd microserviceapp/demo-backend2
         sudo docker build -t docker_backend2img . -f Dockerfile
         sudo docker run docker_backend2img
 
@@ -73,7 +73,7 @@ case $service in
     ssh ubuntu@$ip "sudo apt-get update && sudo apt-get install -y nginx"
     ssh ubuntu@$ip "sudo service nginx start"
     ssh ubuntu@$ip "sudo chown -R ubuntu /var/www/html"
-    scp /home/ubuntu/microserviceapp/demo-frontend/build/index.html ubuntu@$ip:/var/www/html/
+    scp microserviceapp/demo-frontend/build/index.html ubuntu@$ip:/var/www/html/
 
 
 ;;
@@ -81,7 +81,7 @@ case $service in
 "java")
     ssh ubuntu@$ip "sudo apt-get update && sudo apt-get install -y openjdk-11-jdk"
     ssh ubuntu@$ip "sudo git clone "https://github.com/TekspotEdu/microserviceapp.git" "
-    ssh ubuntu@$ip "cd /home/ubuntu/microserviceapp/demo-backend1/target/ && sudo java -jar sentiment-analysis-web-0.0.2-SNAPSHOT.jar --sa.logic.api.url http://$ip:5000"
+    ssh ubuntu@$ip "microserviceapp/demo-backend1/target/ && sudo java -jar sentiment-analysis-web-0.0.2-SNAPSHOT.jar --sa.logic.api.url http://$ip:5000"
 
 
 ;;
@@ -92,7 +92,7 @@ case $service in
     ssh ubuntu@$ip "sudo git clone "https://github.com/TekspotEdu/microserviceapp.git" "
 #     ssh ubuntu@$ip "sudo chmod 744 microserviceapp"
 #     ssh ubuntu@$ip "sudo chown -R ubuntu /microserviceapp/demo-backend2/sa"
-    ssh ubuntu@$ip "cd /home/ubuntu/microserviceapp/demo-backend2/sa/ && sudo pip3 install -r requirements.txt && sudo python3 sentiment_analysis.py"
+    ssh ubuntu@$ip "cd microserviceapp/demo-backend2/sa/ && sudo pip3 install -r requirements.txt && sudo python3 sentiment_analysis.py"
 ;;
 
 esac
